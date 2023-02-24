a6766c1c68da7edba5022c2f888d539cab2733bb
falk-env <dimitri.falk1@gmail.com>
2023-02-24 17:14:44 +0100

    init

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Data: Polygon representation of municipalities in Germany (VG250 31.12.2021)
+#'
+#' VG250_GEM product provided by the Federal Agency for Cartography and Geodesy, Germany
+#'
+#' @format Simple feature collection of type MULTIPOLYGON with 11,123 features and 26 field:
+#' \describe{
+#'   \item{GEN}{municipality name}
+#'   \item{geometry}{centroid coordinates}
+#' }
+#' @source <https://daten.gdz.bkg.bund.de/produkte/vg/vg250_ebenen_1231/aktuell/vg250_12-31.utm32s.shape.ebenen.zip>
+#' @note Last access: 2023-02-24
+#' @description <https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-250-000-stand-31-12-vg250-31-12.html>
+#' @note License: Data licence Germany – attribution – version 2.0
+#' @note Copyright: GeoBasis-DE / BKG 2022 (modified)
+"vg250_gem"
