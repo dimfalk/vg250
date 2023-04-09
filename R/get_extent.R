7c0d9f51fbf6ce0d653b28a0c6404cd2d4e15d01
falk-env <dimitri.falk1@gmail.com>
2023-04-09 12:03:46 +0200

    enh(get_extent): drop as_bbox again

--- R/get_extent.R
+++ R/get_extent.R
@@ -2,19 +2,15 @@
 #'
 #' @param x character. Municipality name.
 #' @param level character. Relevant administrative level.
-#' @param as_bbox logical. Return bounding box as polygon or xmin/ymin/xmax/ymax?
 #'
-#' @return Geometry set of class `sfc_POLYGON` or extent of class `bbox`.
+#' @return Geometry set of class `sfc_POLYGON`.
 #' @export
 #'
 #' @examples
 #' get_extent(x = "Aachen")
 #' get_extent(x = "Städteregion Aachen", level = "KRS")
-#'
-#' get_extent(x = "Aachen", as_bbox = TRUE)
 get_extent <- function(x = NULL,
-                       level = "GEM",
-                       as_bbox = FALSE) {
+                       level = "GEM") {
 
   # check arguments ------------------------------------------------------------
 
@@ -24,18 +20,11 @@ get_extent <- function(x = NULL,
 
   checkmate::assert_choice(level, allowed_level)
 
-  checkmate::assert_logical(as_bbox)
-
   # ----------------------------------------------------------------------------
 
   bbox <- dplyr::filter(vg250, get(level) == x) |>
     sf::st_bbox() |>
     sf::st_as_sfc()
-
-  if (as_bbox == TRUE) {
-
-    bbox <- sf::st_bbox(bbox)
-  }
 
   bbox
 }
