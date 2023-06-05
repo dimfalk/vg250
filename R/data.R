94df6e595b0e9340a2ec10826ed6300350b22d28
Dimitri Falk <dimitri.falk1@gmail.com>
2023-06-05 17:43:45 +0200

    data(vg250): population added
    #11

--- R/data.R
+++ R/data.R
@@ -1,17 +1,18 @@
-#' Data: Polygon representation of administrative areas in Germany (VG250 31.12.2021)
+#' Data: Polygon representation of administrative areas in Germany (VG250-EW 31.12.2021)
 #'
 #' VG250 product provided by the Federal Agency for Cartography and Geodesy, Germany
 #'
-#' @format Simple feature collection of type MULTIPOLYGON with 11,123 features and 3 fields:
+#' @format Simple feature collection of type MULTIPOLYGON with 11,123 features and 4 fields:
 #' \describe{
 #'   \item{GEM}{municipality name, "GEM" level}
 #'   \item{KRS}{district name, "KRS" level}
 #'   \item{LAN}{state name, "LAN" level}
-#'   \item{geometry}{coordinates}
+#'   \item{EWZ}{number of inhabitants}
+#'   \item{geom}{coordinates}
 #' }
-#' @source <https://daten.gdz.bkg.bund.de/produkte/vg/vg250_ebenen_1231/aktuell/vg250_12-31.utm32s.shape.ebenen.zip>
-#' @note Last access: 2023-04-08
-#' @description <https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-250-000-stand-31-12-vg250-31-12.html>
+#' @source <https://daten.gdz.bkg.bund.de/produkte/vg/vg250-ew_ebenen_1231/aktuell/vg250-ew_12-31.utm32s.shape.ebenen.zip>
+#' @note Last access: 2023-06-05
+#' @description <https://gdz.bkg.bund.de/index.php/default/verwaltungsgebiete-1-250-000-mit-einwohnerzahlen-stand-31-12-vg250-ew-31-12.html>
 #' @note License: Data licence Germany – attribution – version 2.0
 #' @note Copyright: GeoBasis-DE / BKG 2022 (modified)
 "vg250"
--- data-raw/vg250.R
+++ data-raw/vg250.R
@@ -1,8 +1,8 @@
 ## code to prepare `vg250` dataset goes here
 
-file <- "vg250_12-31.utm32s.shape.ebenen.zip"
+file <- "vg250-ew_12-31.utm32s.shape.ebenen.zip"
 
-base_url <- paste0("https://daten.gdz.bkg.bund.de/produkte/vg/vg250_ebenen_1231/aktuell/", file)
+base_url <- paste0("https://daten.gdz.bkg.bund.de/produkte/vg/vg250-ew_ebenen_1231/aktuell/", file)
 
 utils::download.file(base_url, file)
 
@@ -10,22 +10,19 @@ utils::unzip(file)
 
 unlink(file)
 
-
+# read data --------------------------------------------------------------------
 
 gem <- list.files(pattern = "VG250_GEM.shp", full.names = TRUE, recursive = TRUE)
 krs <- list.files(pattern = "VG250_KRS.shp", full.names = TRUE, recursive = TRUE)
 lan <- list.files(pattern = "VG250_LAN.shp", full.names = TRUE, recursive = TRUE)
 
-vg250_gem <- sf::read_sf(gem) |>
-  dplyr::select("GEN")
+vg250_gem <- sf::read_sf(gem) |> dplyr::select("GEN", "EWZ")
 
-vg250_krs <- sf::read_sf(krs) |>
-  dplyr::select("GEN")
+vg250_krs <- sf::read_sf(krs) |> dplyr::select("GEN")
 
-vg250_lan <- sf::read_sf(lan) |>
-  dplyr::select("GEN")
+vg250_lan <- sf::read_sf(lan) |> dplyr::select("GEN")
 
-
+# processing -------------------------------------------------------------------
 
 vg250_gemkrs <- sf::st_join(sf::st_centroid(vg250_gem), vg250_krs)
 
@@ -36,11 +33,14 @@ geom <- sf::st_geometry(vg250_gem)
 atab <- sf::st_drop_geometry(vg250_gemkrslan) |>
   dplyr::rename("GEM" = "GEN.x",
                 "KRS" = "GEN.y",
-                "LAN" = "GEN")
+                "LAN" = "GEN") |>
+  dplyr::select("GEM", "KRS", "LAN", "EWZ")
 
 vg250 <- sf::st_as_sf(atab, geom) |>
   sf::st_transform("epsg:4326") |>
   sf::st_make_valid()
+
+# write to disk ----------------------------------------------------------------
 
 usethis::use_data(vg250, overwrite = TRUE)
 
