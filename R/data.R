6c3bde9baaeccbc69db9fb2bb8227af10434f7d6
falk-env <dimitri.falk1@gmail.com>
2024-04-03 12:59:30 +0200

    doc(vg250): data types

--- R/data.R
+++ R/data.R
@@ -4,12 +4,11 @@
 #'
 #' @format Simple feature collection of type MULTIPOLYGON with 10,994 features and 5 fields:
 #' \describe{
-#'   \item{GEM}{municipality name, "GEM" level}
-#'   \item{KRS}{district name, "KRS" level}
-#'   \item{LAN}{state name, "LAN" level}
-#'   \item{EWZ}{number of inhabitants according to census bureau \code{[-]}}
-#'   \item{KFL}{area according to cadastral land register \code{[km2]}}
-#'   \item{geom}{coordinates}
+#'   \item{GEM}{character. Municipality name, "GEM" level.}
+#'   \item{KRS}{character. District name, "KRS" level.}
+#'   \item{LAN}{character. State name, "LAN" level.}
+#'   \item{EWZ}{integer. Number of inhabitants according to census bureau \code{[-]}.}
+#'   \item{KFL}{numeric. Area according to real estate cadastre \code{[km2]}.}
 #' }
 #' @source <https://daten.gdz.bkg.bund.de/produkte/vg/vg250-ew_ebenen_1231/aktuell/vg250-ew_12-31.utm32s.shape.ebenen.zip>
 #' @note Last access: 2023-06-05
