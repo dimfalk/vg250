9235d34246ea1c81f7c3a86d2a5a1516d538a016
Dimitri Falk <dimitri.falk1@gmail.com>
2026-03-06 19:20:42 +0100

    doc(vg250): roxygen tags

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Data: Polygon representation of administrative areas in Germany (VG250-EW 31.12.2024)
 #'
-#' VG250 product provided by the Federal Agency for Cartography and Geodesy, Germany
+#' @description VG250 product provided by the Federal Agency for Cartography and Geodesy, Germany
 #'
 #' @format Simple feature collection of type MULTIPOLYGON with 10,978 features and 6 fields:
 #' \describe{
@@ -12,9 +12,13 @@
 #'
 #'   \item{geom}{sfc_GEOMETRY. Coordinates.}
 #' }
-#' @source <https://daten.gdz.bkg.bund.de/produkte/vg/vg250-ew_ebenen_1231/aktuell/vg250-ew_12-31.utm32s.shape.ebenen.zip>
+#'
 #' @note Last access: 2025-12-20
-#' @description <https://gdz.bkg.bund.de/index.php/default/verwaltungsgebiete-1-250-000-mit-einwohnerzahlen-stand-31-12-vg250-ew-31-12.html>
 #' @note License: Data licence Germany – attribution – version 2.0
 #' @note Copyright: GeoBasis-DE / BKG 2025 (modified)
+#'
+#' @source Data from the Federal Agency for Cartography and Geodesy (BKG):
+#'   <https://daten.gdz.bkg.bund.de/produkte/vg/vg250-ew_ebenen_1231/aktuell/vg250-ew_12-31.utm32s.shape.ebenen.zip>
+#'
+#' @references Product page: <https://gdz.bkg.bund.de/index.php/default/verwaltungsgebiete-1-250-000-mit-einwohnerzahlen-stand-31-12-vg250-ew-31-12.html>
 "vg250"
